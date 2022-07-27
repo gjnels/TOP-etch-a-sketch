6cf847f1d8cdcd74c162528c93f57a7c41e7a7cf
garrett <gnelson@duck.com>
2022-07-27 12:06:56 -0400

    added option to change number of grid squares

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 "use strict";
 
+// select html elements
 const grid = document.querySelector(".grid"); // container for etch a sketch grid
-
-// const
+const gridSizeDisplay = document.querySelector(".grid-size > p > span");
+const gridSizeInput = document.querySelector(".grid-size > input");
 
 const gridDimension = "30rem";
-const gridSize = 16; // will change this later to be adjustable by user
+
+const gridSizeMin = 2;
+const gridSizeMax = 100;
+const defaultGridSize = 16; // will change this later to be adjustable by user
 const cellSize = "1fr";
 
+// give grid a width and height
 grid.style.width = gridDimension;
 grid.style.height = gridDimension;
 
-// grid cells depend on grid size
-// cell size based on root font-size
-grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize})`;
-grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize})`;
+// set grid size option defaults
+gridSizeInput.min = `${gridSizeMin}`;
+gridSizeInput.max = `${gridSizeMax}`;
+gridSizeInput.defaultValue = `${defaultGridSize}`;
 
-// add square to the grid
-for (let i = 0; i < gridSize ** 2; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  grid.appendChild(square);
+// event listener callback functions
+function addSquaresToGrid(size) {
+  for (let i = 0; i < size ** 2; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+  }
 }
 
 function fillSquare(e) {
@@ -31,7 +38,40 @@ function eraseSquare(e) {
   e.target.classList.remove("filled");
 }
 
-document.querySelectorAll(".square").forEach((square) => {
-  square.addEventListener("mousedown", fillSquare);
-  square.addEventListener("mouseover", fillSquare);
-});
+function clearGrid() {
+  document.querySelectorAll(".square").forEach((square) => {
+    square.classList.remove("filled");
+  });
+}
+
+function setGridSize(e) {
+  clearGrid();
+
+  const size = +e?.target.value || defaultGridSize;
+  grid.style.gridTemplateColumns = `repeat(${size}, ${cellSize})`;
+  grid.style.gridTemplateRows = `repeat(${size}, ${cellSize})`;
+  addSquaresToGrid(size);
+  attachSquareEventListeners();
+}
+
+function setGridSizeDisplay(e) {
+  const size = +e?.target.value || defaultGridSize;
+  gridSizeDisplay.textContent = `${size} x ${size}`;
+}
+
+function attachSquareEventListeners() {
+  document.querySelectorAll(".square").forEach((square) => {
+    square.addEventListener("mousedown", fillSquare);
+    square.addEventListener("mouseover", fillSquare);
+  });
+}
+
+// add event listeners
+gridSizeInput.addEventListener("change", setGridSize);
+gridSizeInput.addEventListener("input", setGridSizeDisplay);
+
+// init
+setGridSizeDisplay();
+setGridSize();
+addSquaresToGrid(defaultGridSize);
+attachSquareEventListeners();
